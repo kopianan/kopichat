89a279969d8cc2cb3896794f0da2d424c1432a62
kopianan <ananalfredcarlos@gmail.com>
2024-03-02 13:42:21 +0700

    setup get it and injectable

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:kopichat/injectable.dart';
 import 'package:kopichat/presentation/app.dart';
 
-void main() {
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized() ; 
+  configureDependencies(); 
   runApp(const App());
 }
  
