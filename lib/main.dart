ffc956196aa0e0e80e6de8be040d8c7dae987aa1
kopianan <ananalfredcarlos@gmail.com>
2024-03-10 10:06:10 +0700

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:kopichat/injectable.dart';
 import 'package:kopichat/presentation/app.dart';
+import 'package:kopichat/firebase_options.dart'; 
+import 'package:firebase_core/firebase_core.dart'; 
 
-Future<void> main() async{
-  WidgetsFlutterBinding.ensureInitialized() ; 
-  configureDependencies(); 
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  configureDependencies();
   runApp(const App());
 }
- 
